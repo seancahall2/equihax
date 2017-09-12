44331da8c70d2645ee4f0ccf659407a3a5178b39
seancahall <sean@greekrush.com>
2017-09-12 12:05:28 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,131 @@
+const express = require("express");
+const viewEngine = require("ejs-mate");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const index = require("./routes/index");
+const accounts = require("./routes/accounts");
+const transfers = require("./routes/transfers");
+const auth = require("./routes/auth");
+const register = require("./routes/register");
+const user = require("./routes/user");
+const session = require("express-session");
+const SQLiteStore = require("connect-sqlite3")(session);
+const { flashMiddleware } = require("./flash");
+// const Db = require('./db');
+// console.log(Db.instance);
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.engine("ejs", viewEngine);
+app.set("view options", { layout: true });
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, "..", "public", "favicon.ico")));
+app.use(logger("dev"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser("secret"));
+app.use(
+  session({
+    name: "strawbank",
+    cookie: { maxAge: 60000 },
+    store: new SQLiteStore({
+      dir: path.join(__dirname, "..", "db"),
+      db: "development.sqlite",
+      table: "sessions"
+    }),
+    saveUninitialized: true,
+    resave: "true",
+    secret: "secret"
+  })
+);
+
+app.use(flashMiddleware);
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use(function(req, res, next) {
+  res.locals.sessionFlash = req.session.sessionFlash;
+  delete req.session.sessionFlash;
+  next();
+});
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.session.currentUser || null;
+  next();
+});
+
+app.use("/", index);
+app.use("/accounts", accounts);
+app.use("/transfers", transfers);
+app.use("/auth", auth);
+app.use("/register", register);
+app.use("/user", user);
+
+// Content Security Policy
+var csp = require("helmet-csp");
+
+app.use(
+  csp({
+    // Specify directives as normal.
+    directives: {
+      defaultSrc: ["'self'", "default.com"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["style.com", "'unsafe-inline'"],
+      fontSrc: ["'self'"],
+      imgSrc: ["img.com", "data:"],
+      sandbox: ["allow-forms", "allow-scripts"],
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: true
+    },
+
+    // This module will detect common mistakes in your directives and throw errors
+    // if it finds any. To disable this, enable "loose mode".
+    loose: false,
+
+    // Set to true if you only want browsers to report errors, not block them.
+    // You may also set this to a function(req, res) in order to decide dynamically
+    // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
+    reportOnly: false,
+
+    // Set to true if you want to blindly set all headers: Content-Security-Policy,
+    // X-WebKit-CSP, and X-Content-Security-Policy.
+    setAllHeaders: false,
+
+    // Set to true if you want to disable CSP on Android where it can be buggy.
+    disableAndroid: false,
+
+    // Set to false if you want to completely disable any user-agent sniffing.
+    // This may make the headers less compatible but it will be much faster.
+    // This defaults to `true`.
+    browserSniff: true
+  })
+);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
